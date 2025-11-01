e015be8516637d31f76f76d5b44427f294d5f2b8
mmije0ng <victon777@naver.com>
2025-11-01 16:54:38 +0900

    test(domain): Lotto 테스트 패키지 이동 및 테스트 케이스 추가
    - LottoTest를 lotto.domain 패키지로 이동
    - 기존 테스트 케이스 유지 (번호 개수, 중복 검증)
    - 추가 테스트 케이스 구현
      - 번호 범위 검증 (1 미만, 45 초과)
      - 번호 개수 검증 (6개 미만)
      - 오름차순 정렬 검증
      - 일치 번호 개수 계산 검증
      - 보너스 번호 포함 여부 검증

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -10,12 +10,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
-    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다")
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(MessageConstants.ERROR_LOTTO_NUMBER_COUNT);
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 번호의 개수가 6개 미만이면 예외가 발생한다")
@@ -24,14 +29,6 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(MessageConstants.ERROR_LOTTO_NUMBER_COUNT);
-    }
-
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다")
-    @Test
-    void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(MessageConstants.ERROR_DUPLICATE_NUMBER);
     }
 
     @DisplayName("로또 번호가 1 미만이면 예외가 발생한다")
@@ -82,4 +79,3 @@ class LottoTest {
         assertThat(lotto.containsNumber(7)).isFalse();
     }
 }
-
